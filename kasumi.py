6237e581e112d4e71454c4c2f03acd5ad6261ee0
lulud41 <lulud41@gmail.com>
2020-11-11 20:57:39 +0100

    fonction lecture bloc OK

--- kasumi.py
+++ kasumi.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
 def lecture_bloc(file, bloc_size):
-    while  True:
+    last_iteration = False
+
+    while  not last_iteration:
         data = file.read(bloc_size)
 
         if len(data) < bloc_size:
             data = data + b"0"*(bloc_size-len(data))
-
-        if data == b'':
-            break
+            last_iteration = True
 
         yield data
 
 if __name__ == '__main__':
 
     with open("fichier_clair", "rb") as file:
-        for i in test(file, 32):
-            print(i)
+        for i in lecture_bloc(file, 32):
+            print(len(i))
             input()
