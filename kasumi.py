91c2d92590695e1e54575e3ab2c4a5677aa1b540
lulud41 <lulud41@gmail.com>
2020-11-15 21:18:36 +0100

    encryption kasumi ok

--- kasumi.py
+++ kasumi.py
@@ -8,7 +8,6 @@ KEY_BIT_SIZE = 128
 
 NOTHING_UP_NUMBER = 0x123456789ABCDEFFEDCBA9876543210
 
-
 S_BOX_1 = [187,  67,  47,   2, 159, 168, 194,  29, 168, 241,  29,  65,  36,
         95,  85, 240,  84, 252,  58, 172, 223, 232, 216, 106,  62,  41,
        159,   1, 212, 239, 199,  91,   9,  97, 112, 214, 138, 136, 185,
@@ -66,6 +65,7 @@ S_BOX_2 = [187,  97,  53, 139, 159,  88,   6, 174, 251, 115, 118, 237, 187,
 
     > vaux mieux bosser en little endian
         : permet d'avoir a[0] : octet LSB
+            et MSB a la fin
 
 
     > iteration ; de 0 à 7,
@@ -75,15 +75,18 @@ S_BOX_2 = [187,  97,  53, 139, 159,  88,   6, 174, 251, 115, 118, 237, 187,
 
 
 """
-    générer avec RC4, clé de 128 bits
+    à générer avec RC4, clé de 128 bits
 
 """
 def generate_keys():
     key = random.randint(2**(KEY_BIT_SIZE-1), 2**KEY_BIT_SIZE-1)
-    modifed_key = key ^ NOTHING_UP_NUMBER
+    modified_key = key ^ NOTHING_UP_NUMBER
 
-    return key, modifed_key
+    return key, modified_key
 
+"""
+    récup une portion de 16 bits de la clé de 128
+"""
 def take_sub_key(key, sub_key_index):
     key = key.to_bytes(KEY_BIT_SIZE//8, "little")
     key = key[(sub_key_index)*2 : (sub_key_index+1)*2]
@@ -98,11 +101,11 @@ def generate_sub_key(key, modified_key, iteration):
 
     keys_dict = {}
 
-    keys_dict["KL_i_1"] = left_shift( take_sub_key(key, iteration), 1)
+    keys_dict["KL_i_1"] = left_shift( take_sub_key(key, iteration), 1, 16)
     keys_dict["KL_i_2"] = take_sub_key(modified_key, (iteration+2)%8 )
 
     keys_dict["KO_i_1"] = left_shift( take_sub_key(key, (iteration+1)%8) , 5)
-    keys_dict["KO_i_1"] = left_shift( take_sub_key(key, (iteration+5)%8), 8)
+    keys_dict["KO_i_2"] = left_shift( take_sub_key(key, (iteration+5)%8), 8)
     keys_dict["KO_i_3"] = left_shift( take_sub_key(key, (iteration+6)%8), 13)
 
     keys_dict["KI_i_1"] = take_sub_key(modified_key, (iteration+4)%8)
@@ -111,16 +114,15 @@ def generate_sub_key(key, modified_key, iteration):
 
     return keys_dict
 
-def right_shift(int, i):
-    length = int.bit_length()
+def right_shift(int, i, nb_bits=16):
 
-    result = ( int >> i) | (( int & (2** i - 1)) << (length - i))
+    result = ( int >> i) | (( int & (2** i - 1)) << (nb_bits - i))
     return result
 
-def left_shift(int, i):
-    length = int.bit_length()
+def left_shift(int, i, nb_bits=16):
+    #print(length, i)
 
-    result = (int & (2**(length - i) - 1) ) << i  | (int >> (length - i))
+    result = (int & (2**(nb_bits - i) - 1) ) << i  | (int >> (nb_bits - i))
     return result
 
 def lecture_bloc(file, byte_bloc_size):
@@ -137,36 +139,101 @@ def lecture_bloc(file, byte_bloc_size):
 def FI(y, z):
 
     z_bytes = z.to_bytes(2, "little")
-    z_1 = z_bytes[0]
-    z_2 = z_bytes[1]
+    z_1 = z_bytes[0] # LSB
+    z_2 = z_bytes[1] # MSB
 
     z_1 = S_BOX_1[z_1]
     z_2 = S_BOX_2[z_2]
 
-    z = z_2.to_bytes(1,"little") + z_1.to_bytes(1,"little")
+
+    z = z_1.to_bytes(1,"little") + z_2.to_bytes(1,"little")
     z = int.from_bytes(z, "little")
 
-    x = right_shift(y, 2) ^ (z)
+    x = right_shift(y, 2, nb_bits=8) ^ (z)
 
     return x
 
+def FL(x, keys_dict):
+    x_bytes = x.to_bytes(4, "little")
 
+    l = x_bytes[:2] # LSB
+    r = x_bytes[2:] # MSB
 
+    l = int.from_bytes(l, "little")
+    r = int.from_bytes(r, "little")
 
+    r_dash = left_shift( l and keys_dict["KL_i_1"] , 1 ) ^ r
+    l_dash = left_shift( r_dash or keys_dict["KL_i_2"] , 1 ) ^ l
+
+    r_dash = r_dash.to_bytes(2, "little")
+    l_dash = l_dash.to_bytes(2, "little")
+
+    x_dash = int.from_bytes(l_dash + r_dash, "little")
+
+    return x_dash
+
+def FO(x, keys_dict):
+    x_bytes = x.to_bytes(4, "little")
+
+    l = x_bytes[:2] # LSB
+    r = x_bytes[2:] # MSB
+
+    l = int.from_bytes(l, "little")
+    r = int.from_bytes(r, "little")
+
+    for j in range(1,4):
+        new_r = FI(keys_dict["KI_i_"+str(j)], l ^ keys_dict["KO_i_"+str(j)] ) ^ r
+        l = r
+        r = new_r
+
+    l = l.to_bytes(2,"little")
+    r = r.to_bytes(2,"little")
+
+    x_dash = int.from_bytes(l+r, "little")
+
+    return x_dash
+
+def kasumi_feistel_encryption(clear_block, key, modified_key):
+
+    #x_bytes = clear_block.to_bytes(8, "little")
+
+    L = clear_block[:4] # LSB
+    R = clear_block[4:] # MSB
+
+    L = int.from_bytes(L,"little")
+    R = int.from_bytes(R,"little")
+
+    for iteration in range(0, 8):
+
+        keys_dict = generate_sub_key(key, modified_key, iteration)
+
+        # even
+        if iteration % 2 == 0:
+            new_L = FO( FL(L, keys_dict), keys_dict) ^ R
+            R = L
+            L = new_L
+
+        #odd
+        if iteration %2 == 1:
+            new_L = FL( FO(L ,keys_dict) ,keys_dict) ^ R
+            R = L
+            L = new_L
+
+    L = L.to_bytes(4,"little")
+    R = R.to_bytes(4,"little")
+
+    crypted_block = int.from_bytes(L+R, "little")
+
+    return crypted_block
 
 if __name__ == '__main__':
-    """
+
+    key, modified_key = generate_keys()
+
     with open("fichier_clair", "rb") as file:
-        for i in lecture_bloc(file, BLOC_SIZE_BYTES):
-            print(bin(int.from_bytes(i, "big")))
-            break"""
 
-    key, modifed_key = generate_keys()
+        for clear_block in lecture_bloc(file, BLOC_SIZE_BYTES):
 
-    for i in range(0,8):
-        a = generate_sub_key(key, modifed_key, i)
-        for j in a.keys():
-            print(a[j].bit_length())
-
-
-        input()
+            c = kasumi_feistel_encryption(clear_block, key, modified_key)
+            print(clear_block,"   => ", c)
+            input()
